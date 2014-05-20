2c123e39eb889a758f10f3776d1df1c6a9140853
Pei Qing 卿培 <edwardtoday@gmail.com>
2014-05-20 13:01:50 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,3 @@
-function onload() {
-	// Your onload code goes here to initialize your page.
-	// Finally, schedule a call for a second round of low-priority initialization.
-	window.setTimeout(afterload, 1);
-}
-
-function afterload() {
-	// Called a few ms after the page has loaded.
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-}
-
-var _gaq = _gaq || [];
-var pluginUrl = 
- '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-_gaq.push(['_setAccount', 'UA-29345258-1']);
-_gaq.push(['_trackPageview']);
-
 // Reset dynamo span height
 $(window)
 	.resize(function() {
